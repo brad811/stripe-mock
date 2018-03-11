77519bd79c962b313f34749512bbd25a731bb554
Brad Jewell <brad811@gmail.com>
2018-03-11 14:57:39 -0500

    add ec2 instance to terraform

--- default.tf
+++ default.tf
@@ -1,3 +1,24 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "stripe-mock" {
+    count = 1
+
+    instance_type = "t2.micro"
+    ami = "ami-07585467"
+    associate_public_ip_address = true
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get install -y docker",
+            "sudo service docker start",
+            "sudo docker pull brad811/stripe-mock:master",
+            "sudo docker run -d stripe-mock"
+        ]
+    }
+
+    tags {
+        Name = "Terraform stripe-mock ${count.index}"
+    }
+}
