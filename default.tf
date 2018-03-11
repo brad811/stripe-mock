7e9cf1c5bad1aedb46cccf650fc950d95e2b92ee
Brad Jewell <brad811@gmail.com>
2018-03-11 18:19:34 -0500

    add key pair

--- default.tf
+++ default.tf
@@ -2,12 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_key_pair" "stripe-mock" {
+  key_name   = "stripe-mock"
+  public_key = "${var.key_pair}"
+}
+
 resource "aws_instance" "stripe-mock" {
     count = 1
 
     instance_type = "t2.micro"
     ami = "ami-66506c1c"
     associate_public_ip_address = true
+    key_name = "stripe-mock"
 
     provisioner "remote-exec" {
         inline = [
