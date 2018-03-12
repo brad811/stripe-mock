c266fa4744229a8478b1966898999ab5a889c706
Brad Jewell <brad811@gmail.com>
2018-03-12 14:53:51 -0500

    add s3 backend

--- default.tf
+++ default.tf
@@ -2,13 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "key_pair" {
-  type = "string"
+terraform {
+  backend "s3" {
+    bucket = "brad811-terraform-state"
+    key    = "stripe-mock.tfstate"
+    region = "us-east-1"
+  }
 }
 
 resource "aws_key_pair" "stripe-mock" {
   key_name   = "stripe-mock"
-  public_key = "${var.key_pair}"
+  public_key = "${file("${path.module}/ec2.pub")}"
 }
 
 resource "aws_instance" "stripe-mock" {
@@ -18,6 +22,11 @@ resource "aws_instance" "stripe-mock" {
     ami = "ami-66506c1c"
     associate_public_ip_address = true
     key_name = "stripe-mock"
+
+    connection {
+        user = "ec2-user"
+        private_key = "${file("${path.module}/ec2.key")}"
+    }
 
     provisioner "remote-exec" {
         inline = [
